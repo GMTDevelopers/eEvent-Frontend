7297e1f4e86248a583691581793d1b4dc4eaffdf
Gmt Dev <gmtdev@gmail.com>
2026-04-15 12:47:48 +0100

    homePage responsiveness done 1.o

--- src/app/footer/footer.jsx
+++ src/app/footer/footer.jsx
@@ -31,12 +31,12 @@ const Footer = () => {
                 <div className={`${styles.footer2Txt} ${styles.section1}`}>
                     <div className={styles.f2Pack1}>
                         <Image className={`${styles.f2PackHeader} ${styles.footerLogo}`} alt='footer logo' src="/images/logo.webp" width={114} height={29} />
-                        <p className={styles.footer2Txt}>The online marketplace that connects customers with trusted event vendors for everything from catering and rentals to music and décor. We make event planning simple, secure, and stress-free by bringing all your event needs into one reliable platform.</p>
+                        <p /* className={styles.footer2Txt} */>The online marketplace that connects customers with trusted event vendors for everything from catering and rentals to music and décor. We make event planning simple, secure, and stress-free by bringing all your event needs into one reliable platform.</p>
                         <div className={styles.termOfUse}>
-                            <p style={{color:"#82027D"}} className={styles.footer2Txt}>Privacy Policy    |   Terms of Use</p>
+                            <p style={{color:"#82027D"}} /* className={styles.footer2Txt} */>Privacy Policy    |   Terms of Use</p>
                         </div>
                     </div>
-                    <div className={styles.f2Pack2}>
+                    <div className={`${styles.f2Pack2} ${styles.f2Pack22}`}>
                         <p className={styles.f2PackHeader}>CATEGORIES</p>                        
                         <ul>
                             <li>Catering</li>
@@ -89,7 +89,7 @@ const Footer = () => {
                     
                 </div>
                 <div className={styles.copyRight}>
-                    <p className={styles.footer2Txt}>Copyright © 2025 eEvents. All Rights Reserved. No part of this website, including text, design, or content, may be copied, reproduced, or used without written permission.</p>
+                    <p >Copyright © 2025 eEvents. All Rights Reserved. No part of this website, including text, design, or content, may be copied, reproduced, or used without written permission.</p>
                 </div>
             </div>
         </div>
